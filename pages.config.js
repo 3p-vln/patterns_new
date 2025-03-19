178acc21dc9d88e72ee4802d1bafd1cd87c4329d
3p-vln <vldknovak@gmail.com>
2025-03-19 18:12:51 +0200

    fix for deploy

--- pages.config.js
+++ pages.config.js
@@ -6,48 +6,16 @@ const pages = [
     path: resolve(__dirname, 'index.html'),
   },
   {
-    name: 'quiz',
-    path: resolve(__dirname, 'quiz.html'),
+    name: 'factory',
+    path: resolve(__dirname, 'factory.html'),
   },
   {
-    name: 'personal-pack',
-    path: resolve(__dirname, 'personal-pack.html'),
+    name: 'builder',
+    path: resolve(__dirname, 'builder.html'),
   },
   {
-    name: 'shop',
-    path: resolve(__dirname, 'shop.html'),
-  },
-  {
-    name: 'one-product',
-    path: resolve(__dirname, 'one-product.html'),
-  },
-  {
-    name: 'registration',
-    path: resolve(__dirname, 'registration.html'),
-  },
-  {
-    name: 'login',
-    path: resolve(__dirname, 'login.html'),
-  },
-  {
-    name: 'recover-password',
-    path: resolve(__dirname, 'recover.html'),
-  },
-  {
-    name: 'reset-password',
-    path: resolve(__dirname, 'reset-password.html'),
-  },
-  {
-    name: 'profile',
-    path: resolve(__dirname, 'profile.html'),
-  },
-  {
-    name: 'create-order',
-    path: resolve(__dirname, 'create-order.html'),
-  },
-  {
-    name: 'successful-order',
-    path: resolve(__dirname, 'successful-order.html'),
+    name: 'abstract-factory',
+    path: resolve(__dirname, 'abstract-factory.html'),
   },
 ];
 
